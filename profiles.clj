443174cbd36b23cdc59862bc7413b78435d94377
Hugo Duncan <hugo@hugoduncan.org>
2014-08-28 15:32:55 -0400

    Add travis and release configuration

--- profiles.clj
+++ profiles.clj
@@ -1,2 +1,3 @@
-{:provided {:dependencies [[org.clojure/clojure "1.6.0"]]}
- :no-subprocess {:modules {:subprocess false}}}
+{:provided {:dependencies [[org.clojure/clojure "1.6.0"]]},
+ :no-subprocess {:modules {:subprocess false}},
+ :dev {:plugins [[lein-pallet-release "RELEASE"]]}}
