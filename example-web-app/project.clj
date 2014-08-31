f1d36a55d703a4cd926aaafe4d75189d2d654949
Hugo Duncan <hugo@palletops.com>
2014-08-31 11:05:16 +0000

    Updated version for next release cycle

--- example-web-app/project.clj
+++ example-web-app/project.clj
@@ -1,4 +1,4 @@
-(defproject com.palletops/bakery-web-app "0.1.0"
+(defproject com.palletops/bakery-web-app "0.1.1-SNAPSHOT"
   :description "A web app with bakery"
   :url "http://palletops.com/example.webapp"
   :license {:name "Eclipse Public License - v 1.0"
