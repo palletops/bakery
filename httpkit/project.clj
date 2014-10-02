43592f049ca6cabe968764673f0314b1325f6e80
Dom Kiva-Meyer <git@domkm.com>
2014-10-02 15:50:58 -0700

    Update httpkit to 2.1.19

--- httpkit/project.clj
+++ httpkit/project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-modules "0.3.9"]]
-  :dependencies [[http-kit "2.1.16"]])
+  :dependencies [[http-kit "2.1.19"]])
