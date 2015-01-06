17469201c75368f368477437769957396a79f1f4
Hugo Duncan <hugo@hugoduncan.org>
2015-01-06 13:37:55 -0500

    Add a core.async channel component
    Adds a leaven component for a core.async channel that can be started and
    stopped.

---
+++ core-async/project.clj
@@ -0,0 +1,17 @@
+(defproject com.palletops/bakery-core-async "0.2.1-SNAPSHOT"
+  :description "A leaven component for a core-async channel."
+  :url "https://github.com/palletops/bakery"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+
+  :resource-paths ["src/cljs" "target/generated/src/cljs"]
+  :source-paths ["src/clj" "target/generated/src/clj"]
+  :plugins [[lein-modules "0.3.9"]]
+  :prep-tasks [["cljx" "once"]]
+  :cljx {:builds [{:source-paths ["src/cljx"]
+                   :output-path "target/generated/src/clj"
+                   :rules :clj}
+                  {:source-paths ["src/cljx"]
+                   :output-path "target/generated/src/cljs"
+                   :rules :cljs}]})
